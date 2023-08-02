f96845105db4827ece68300000b5e68bf13a9a36
672641 <672641@stud.hvl.no>
2023-08-02 03:55:02 -0700

    AAtlasProgram

---
+++ Scripts/python/AtlasProgram/Klient/dxlModDef.py
@@ -0,0 +1,62 @@
+import os
+from dynamixel_sdk import * # Uses Dynamixel SDK library
+
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+# Dynamixel model name
+MY_DXL = 'X_SERIES'       # X330 (5.0 V recommended), X430, X540, 2X430
+
+
+# control table address
+if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
+    ADDR_TORQUE_ENABLE          = 64
+    ADDR_GOAL_POSITION          = 116
+    ADDR_PRESENT_POSITION       = 132
+    DXL_MINIMUM_POSITION_VALUE  = 1500         # Refer to the Minimum Position Limit of product eManual
+    DXL_MAXIMUM_POSITION_VALUE  = 2500      # Refer to the Maximum Position Limit of product eManual 4095
+    BAUDRATE                    = 57600  
+
+# DYNAMIXEL Protocol Version (1.0 / 2.0)
+# https://emanual.robotis.com/docs/en/dxl/protocol2/
+PROTOCOL_VERSION            = 2.0
+
+# Factory default ID of all DYNAMIXEL is 1
+DXL_IDs                      = [1, 2, 3, 4, 5]
+
+# Use the actual port assigned to the U2D2.
+# ex) Windows: "COM*", Linux: "/dev/ttyUSB*", Mac: "/dev/tty.usbserial-*"
+DEVICENAME                  = '/dev/ttyUSB1'
+
+TORQUE_ENABLE               = 1     # Value for enabling the torque
+TORQUE_DISABLE              = 0     # Value for disabling the torque
+DXL_MOVING_STATUS_THRESHOLD = 5    # Dynamixel moving status threshold
+
+index = 0
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
+
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Set the protocol version
+# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+
---
+++ Scripts/python/AtlasProgram/Klient/main.py
@@ -0,0 +1,22 @@
+'''This is the main folder where we import and run the fuctions from the other files 
+whitin the atlas program. This file will be able to run using a U2D2 and a Dynamixel
+chain og 5 motors. The motors are connected in the following order of IDs: 1, 2, 3, 4, 5.
+The motors are connected to the U2D2 which is connected to the computer using a USB cable.
+The input to the motors are the angles of the motors. The angles are calculated using the
+inverse kinematics of the robot. The inverse kinematics are calculated using the forward
+kinematics of the robot. The forward kinematics are calculated using the Denavit-Hartenberg
+parameters of the robot. The Denavit-Hartenberg parameters are calculated using the
+dimensions of the robot. The dimensions of the robot are given in the following file:
+Scripts/python/AtlasProgram/robot_dimensions.py. The dimensions are given in millimeters.
+'''
+from moveR import *
+
+connect()
+
+enableTorque()
+
+moveWithPos(2000, 2000, 2000, 2000, 2000)
+
+closePort()
+
+
